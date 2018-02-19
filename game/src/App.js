8c339fbe48fca0baf4394e98d3507886cdaf8235
Travis Horne <travis.horne03@gmail.com>
2018-02-19 12:04:35 -0500

    attribute update

--- game/src/App.js
+++ game/src/App.js
@@ -16,18 +16,45 @@ class App extends Component {
           <div class="container-fluid">
           <div class="title-arch">Welcome to Reverie <p> "Try not to get yourself killed"</p></div>
         <Card pictureClass=""  paragraph="test" title="BARBARIAN">
-          <p>cruel wicked and inhuman</p>
-           <br/>
-           <p>Attack: 7/10</p>
-           <p>Hit Points: 3/10</p>
-           <p>Speed: 6/10</p>
-           <p>Range: 6/10</p>
+            <hr/>
+            <p>cruel wicked and inhuman</p>
+             <br/>
+             <p>Attack: 7/10</p>
+             <p>Hit Points: 3/10</p>
+             <p>Speed: 6/10</p>
+             <p>Range: 6/10</p>
         </Card>
-        <Card pictureClass="project-2"  paragraph="test" title="PALADIN" />
-        <Card pictureClass="project-3"  paragraph="test" title="MAGE" />
-        <Card pictureClass="project-4"  paragraph="test" title="ROGUE" />
+        <Card pictureClass="project-2"  paragraph="test" title="PALADIN" >
+          <hr/>
+           <p>Champion of a cause.</p>
+           <br/>
+           <p>Attack: 6/10</p>
+           <p>Hit Points: 6/10</p>
+           <p>Speed: 4/10</p>
+           <p>Range: 2/10</p>
+        </Card>
+        <Card pictureClass="project-3"  paragraph="test" title="MAGE" >
+          <hr />
+          <p>Magician or learned person.</p>
+          <br/>
+          <p>Attack: 7/10</p>
+          <p>Hit Points: 3/10</p>
+          <p>Speed: 6/10</p>
+          <p>Range: 6/10</p>
+          </Card>
+        <Card pictureClass="project-4"  paragraph="test" title="ROGUE" >
+          <hr/>
+          <p>Wise & skilled archer.</p>
+          <br/>
+          <p>Attack: 5/10</p>
+          <p>Hit Points: 4/10</p>
+          <p>Speed: 6/10</p>
+          <p>Range: 10/10</p>
+          </Card>
         <div class="clearfix"></div>
             </div>
+
+
               <Footer />
 {/* 	<p className="App-intro">
 			To get started, edit <code>src/App.js</code> and save to reload.
